370f1ba9125f535249bb455f20b772a843f9c603
drakenm <drakenm@gmail.com>
2017-03-13 19:53:22 -0500

    created js outline of injected form - tested and working

--- js/inject-form.js
+++ js/inject-form.js
@@ -10,9 +10,23 @@ if(!window.jQuery)
     script.type = "text/javascript";
     // any cdn will work, google's hosted library is chosen in this instance
     script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js";
+    // inject our form after we dynamically insert jquery library
+    script.onload = injectForm;
     ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( script );
+} else {
+    $(function() {
+        injectForm();
+    });
 }
 
-$(function() {
-   console.log("jQuery is live!");
-});
+function injectForm(options) {
+    // jquery to determine when the document is ready
+    $(function() {
+        var frmEl = document.createElement('form');
+        frmEl.id = options.formID || "nested-form";
+        frmEl.action = options.formAction || "";
+        frmEl.method = "POST";
+        // inject our form
+        document.getElementById('injection-container').appendChild(frmEl);
+    });
+}
